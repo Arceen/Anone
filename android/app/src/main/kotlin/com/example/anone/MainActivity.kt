43d4aec4e9fede6d686d936bf26923c881393aa4
Niloy <niloyman01@gmail.com>
2022-04-12 12:00:20 +0600

    'init

---
+++ android/app/src/main/kotlin/com/example/anone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:anone/constants/routes.dart';
+import 'package:anone/screens/launch_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: launchSplashScreen,
+      routes: {
+        launchSplashScreen: (context) => LaunchScreen(),
+      },
+    );
+  }
+}
